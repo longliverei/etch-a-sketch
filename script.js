7432a16112bd6e912cc20fd2d8611401c04b4d4c
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-26 10:46:50 -0300

    add feat grid range

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-const container = document.querySelector("#container");
+const container = document.querySelector("#grid-container");
+const slider = document.getElementById("grid-slider");
+const value = document.getElementById("output");
 const color = "black";
+const defaultSize = 16;
 
-function makeRows(rows, cols) {
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
-    for (let i = 0; i < (rows * cols); i++) {
-        let square = document.createElement("div");
+const makeGrid = (size) => {
+    container.innerHTML = "";
+    container.style.setProperty("--grid-rows", size);
+    container.style.setProperty("--grid-cols", size);
+
+    for (let i = 0; i < (size * size); i++) {
+        const square = document.createElement("div");
         container.appendChild(square).classList.add("square");
-
-        let squares = document.querySelectorAll(".square");
-        squares.forEach((square) => {
-            square.addEventListener("click", () => {
-                square.setAttribute("style", `background-color: ${color};`);
-            })
-        })
     };
+
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("click", () => {
+            square.setAttribute("style", `background-color: ${color}`)
+        })
+    })
+    
 };
 
-makeRows(16, 16);
+makeGrid(defaultSize);
+
+const update = () => {
+    makeGrid(slider.value);
+}
+
+
+slider.addEventListener("input", update);
