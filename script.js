67dbeff927c0e2385859642a85fdf85860e7bef6
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-27 08:43:22 -0300

    feat active class for button

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ const makeGrid = (size) => {
     };
 };
 
+// Catch color function
 const dye = (e) => {
     e.target.style.backgroundColor = defaultColor;
-}
+};
 
 makeGrid(defaultSize);
 
@@ -25,12 +26,7 @@ makeGrid(defaultSize);
 const colorPicker = document.getElementById("colorPicker");
 colorPicker.addEventListener("input", (e) => {
     defaultColor = e.target.value;
-})
-
-const color = document.getElementById("color");
-color.addEventListener("click", () => {
-    defaultColor = colorPicker.value;
-})
+});
 
 // Slider defines the grid size
 const slider = document.getElementById("grid-slider");
@@ -39,21 +35,38 @@ const value = document.getElementById("output");
 const update = () => {
     value.innerHTML = `${slider.value} x ${slider.value}`;
     makeGrid(slider.value);
-}
+};
 
 value.innerHTML = `${slider.value} x ${slider.value}`;
 
 slider.addEventListener("input", update);
 
+// Color button
+const color = document.getElementById("color");
+color.addEventListener("click", () => {
+    active(color);
+    defaultColor = colorPicker.value;
+});
+
 // Eraser button
 const eraser = document.getElementById("eraser");
 eraser.addEventListener("click", () => {
+    active(eraser);
     defaultColor = `white`;
-})
+});
 
-// clear button
+// Clear button
 const clear = document.getElementById("clear");
 clear.addEventListener("click", () => {
     container.innerHTML = "";
     makeGrid(slider.value);
-})
+});
+
+// Set active class
+const active = (e) => {
+    let elems = document.querySelector(".active");
+    if (elems != null) {
+        elems.classList.remove("active");
+    }
+    e.className = "active";
+}
